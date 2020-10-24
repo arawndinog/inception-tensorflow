807328eccd81ba058eef08c841526fea1c26a7fe
Adrian Wong <adrianwong@astri.org>
2020-10-24 10:26:18 +0800

    add test set

--- training.py
+++ training.py
@@ -7,10 +7,16 @@ from multiprocessing import Queue, Process
 
 def main():
     train_data, train_label = process_dataset.extract_hdf5("/home/adrianwong/Projects/ML_localdata/Dataset/HDF5/CASIA_SC_L_A_label_00000_03880.hdf5")
-    train_indices_partial = train_label<1000
+    train_indices_partial = train_label < 1000
     train_data = train_data[train_indices_partial]
     train_label = train_label[train_indices_partial]
     batch_size = 100
+
+    test_data, test_label = process_dataset.extract_hdf5("/home/adrianwong/Projects/ML_localdata/Dataset/HDF5/CASIA_SC_C_0.hdf5")
+    test_indices_partial = test_label < 1000
+    test_data = test_data[test_indices_partial]
+    test_label = test_label[test_indices_partial]
+    test_data = process_image.reshape_img_batch_to_size(test_data, (224,224))
 
     sess_config = tf.ConfigProto()
     sess_config.gpu_options.allow_growth = True
@@ -42,20 +48,34 @@ def main():
         # print(np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()]))
         saver = tf.train.Saver(max_to_keep=None)
         saver.restore(sess, "outputs/cnn_latest")
-        saver_iter = 1000
+        saver_iter = 5000
+
         train_acc = 0
         train_loss = 100000
+        test_acc = 0
+
         i = 0
         while True:
             train_data_batch, train_label_batch = train_data_q.get()
-            _, train_loss = sess.run([optimizer, cost], feed_dict = {x:train_data_batch, y:train_label_batch, learning_rate: 0.001, is_training:True})
+            _, train_loss = sess.run([optimizer, cost], feed_dict = {x:train_data_batch, y:train_label_batch, learning_rate: 0.0001, is_training:True})
             print("Iteration:", i)
-            print("     Training loss:", train_loss, "Last acc:", train_acc)
+            print("     Training loss:", train_loss, "Last training acc:", train_acc, "Last test acc:", test_acc)
 
             if (i % saver_iter == 0) and (i != 0):
                 train_acc = sess.run(accuracy, feed_dict = {x:train_data_batch, y:train_label_batch, is_training:False})
+
+                test_i = 0
+                test_acc_list = []
+                while test_i < len(test_data):
+                    test_data_batch = test_data[test_i: test_i + batch_size]
+                    test_label_batch = test_label[test_i: test_i + batch_size]
+                    test_batch_acc = sess.run(accuracy, feed_dict = {x:test_data_batch, y:test_label_batch, is_training:False})
+                    test_i += batch_size
+                    test_acc_list.append(test_batch_acc)
+                test_acc = sum(test_acc_list)/len(test_acc_list)
+
                 saver.save(sess, "outputs/cnn_latest", global_step=None)
-                print(train_loss, train_acc)
+                print(train_loss, train_acc, test_acc)
                 print("checkpoint saved")
 
             i += 1
